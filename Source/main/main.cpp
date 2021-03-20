b05ef24c40e4907eb176b002628f6cc087710325
Lettle <1071445082@qq.com>
2021-03-20 19:03:13 +0800

    Success version-dont use

--- Source/main/main.cpp
+++ Source/main/main.cpp
@@ -12,6 +12,15 @@ string KEYWORD[15]={
     "while","for","do",
     "int","string","double","float"};
 
+//常量
+const string SEPARATE = "分隔符";
+const string OPERATE = "运算符";
+const string VAR = "变量名";
+const string KEY = "关键字";
+const string NUM = "数字";
+const string BOOLEAN = "布尔值";
+const string STRING = "字符串";
+
 //分隔符
 char SEPARATER[8]={';',',','{','}','[',']','(',')'};
 //运算符
@@ -92,60 +101,80 @@ int value(T *a,int n,T str){
 }
 
 /**词法分析**/
-map<int,Token> ana(FILE * fpin){
+map<int,Token> ana(FILE * fpin)
+{
+    const string TABLE = "\t\t\t";
+
     char ch=' ';     //取一个字符
     string arr="";   //用ch连接字符串
     Token t; 
     int counter = 0; //记录token数
-    int line = 1;  //当前行号
-    int char_count;//当前字符
+    int line = 0;  //当前行号
+    int char_count;//当前字符位置
     map<int,Token> result; //返回tokens
+
+    string type="None"; //程序当前采集类型
     do{
         //循环获取字符
         ch = fgetc(fpin);
         if(feof(fpin)){break;};//EOF则停止
-
-        arr="";
-        t={};
+        char_count++;
+        arr = "";
         //判断过滤符
+        if(ch=='\n'){
+            line++;char_count=0;
+            continue;} //行数增加
         if(IsFilter(ch)){continue;}
+        
         //判断关键字或变量名
         else if(IsLowLetter(ch)){
             //继续获取小写字母
-            while(IsLowLetter(ch)){
+            while(IsLowLetter(ch+1)){
+                //如果下一位还是小写字母则加入
 				arr += ch;
 				ch=fgetc(fpin);
             }
 			//fseek(fpin,-1L,SEEK_CUR);
 			if(IsKeyword(arr)){
-				cout<<arr<<"\t\t关键字"<<endl;
+				cout<<arr<<TABLE<<KEY<<endl;
 			}
             else{
                 //继续获取大小写字母
-                while(IsLowLetter(ch)||IsUpLetter(ch)){
+                while(IsLowLetter(ch+1)||IsUpLetter(ch+1)){
                     arr += ch;
                     ch = fgetc(fpin);
                 }
-                cout<<arr<<"\t\t变量名"<<endl;
-                /*
-                if(IsSeparater(++ch))
-                    break;
-                */
+                cout <<arr<<TABLE<<VAR<<endl;
             }
+            
+                
         }
         //判断数字
         else if(IsDigit(ch)){
             while(IsDigit(ch)){
                 arr += ch;
-                if(IsSeparater(++ch))
+                /*
+                if(IsSeparater(ch+1))
                     break;
+                    */
                 ch = fgetc(fpin);
                 
             }
-            cout <<arr<<"\t\t数字"<<endl;
+            cout <<arr<<TABLE<<NUM<<endl;
         }
         //判断符号
-        else switch(ch){
+        switch(ch){
+        case '"':
+            {
+                arr += ch;//将双引号加入
+                ch=fgetc(fpin);//跳到下一字符
+                while(ch!='"'){
+                    arr+=ch;ch=fgetc(fpin);
+                }
+                arr += ch;
+                cout <<arr<<TABLE<<STRING<<endl;
+                break;
+            }
         case '+':
         case '-':
         case '*':
@@ -156,49 +185,43 @@ map<int,Token> ana(FILE * fpin){
         case '!':
             {
                 arr += ch;
-                cout<<arr<<"\t\t运算符"<<endl;
+                cout<<arr<<TABLE<<OPERATE<<endl;
                 break;
             }
         case ';':
             {
-                arr += ch;
-                cout <<arr<<"\t\t;"<<endl;
+                cout<<ch<<TABLE<<";"<<endl;
                 break;
             }
         case ',':
         case '(':
         case ')':
+            {
+                cout<<ch<<TABLE<<SEPARATE<<endl;
+                break;
+            }
         case '[':
         case ']':
         case '{':
         case '}':
             {
               arr += ch;
-              cout<<arr<<"\t\t分隔符"<<endl;
+              cout<<arr<<TABLE<<SEPARATE<<endl;
               break;
             }
-        case '"':
+        
+        default :
             {
-                arr += ch;//将双引号加入
-                ch=fgetc(fpin);//跳到下一字符
-                while(ch!='"'){
-                    arr+=ch;
-                    ch=fgetc(fpin);
-                }
-                arr += ch;
-                cout <<arr<<"\t\t字符串"<<endl;
-                break;
+            if(ch!=' ')
+                cout<<"("<<ch<<")"<<":无法识别的字符！"<<endl;
             }
-        default :cout<<"("<<ch<<")"<<":无法识别的字符！"<<endl;
         }
         //if(!arr.empty()){result.insert(pair<int,Token>(counter++,arr))}
-        counter++;
-        //cout <<counter<<endl;
+       
     }while(1);
-    cout <<"count:"<<counter<<endl;
+
     return result;
 }
-
 int main()
 {
     char inFile[40];
@@ -214,3 +237,38 @@ int main()
 
     return 0;
 }
+
+/*
+void paster()
+{
+//模式控制
+        switch(type){
+            case NO:
+            {
+                if(IsDigit(ch)){type = NUM;}
+                else if(IsLowLetter(ch)){type=WD;}
+                else if(IsUpLetter(ch)){type=VAR;}
+                else if(IsOperator(ch)){type=OPERATE;}
+                arr += ch;
+                break;
+            }
+            case KEY:
+                {
+
+                    break;
+                }
+            case VAR:
+                {
+                    break;
+                }
+            case SEPARATE:
+                {
+                    break;
+                }
+            case OEPRATE:
+                {
+                    break;
+                }
+            default: cout <<"default"<<endl;
+        }}
+*/
